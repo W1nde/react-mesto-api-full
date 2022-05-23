2d3310b3dd9d518584da19470e5aedadb8a6d1b4
W1nde <markshadpalov@gmail.com>
2022-05-24 00:54:53 +0700

    cors update

--- backend/app.js
+++ backend/app.js
@@ -38,18 +38,13 @@ const allowedCors = [
   'https://api.mshadpalov.students.nomoredomains.xyz'
 ];
 
-app.use(function (req, res, next) {
+app.use ((req, res, next) => {
   const { origin } = req.headers;
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', true);
-  }
   const { method } = req;
-
   const requestHeaders = req.headers['access-control-request-headers'];
-
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-
+  res.header('Access-Control-Allow-Origin', origin);
+  res.header('Access-Control-Allow-Credentials', true);
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
