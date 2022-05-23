9ca8b81944d4daaa1882fbca0ba557ef350c2918
W1nde <markshadpalov@gmail.com>
2022-05-24 00:59:34 +0700

    +

--- backend/app.js
+++ backend/app.js
@@ -45,7 +45,7 @@ app.use ((req, res, next) => {
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   res.header('Access-Control-Allow-Origin', origin);
   res.header('Access-Control-Allow-Credentials', true);
-  if (method === 'OPTIONS') {
+  if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
     return res.end();
